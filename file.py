04ecb5d6bc8bf76c6a7a9ea7876abc3d4e23bf91
Abdi Deresa <Abdishegute@gmail.com>
2024-08-24 09:55:21 -0700

    added another ball

--- file.py
+++ file.py
@@ -15,8 +15,11 @@ canvas = Canvas(window, width=WIDTH, height=HEIGHT)
 canvas.pack()
 
 volley_ball = Ball(canvas,0,0,100,1,1,"red")
+tennis_ball = Ball(canvas,0,0,50,4,3,"green")
+
 while True:
     volley_ball.move()
+    tennis_ball.move()
     window.update()
     time.sleep(0.01)
 
